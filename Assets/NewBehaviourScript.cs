626f0f28f7580e7f7a2690804e3ec7620d93f375
DESKTOP-18CQA57\user <test@test.com>
2017-04-26 23:00:27 +0900

    add script to set screen size.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    void Awake()
+    {
+        Screen.SetResolution(1024, 500, false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
